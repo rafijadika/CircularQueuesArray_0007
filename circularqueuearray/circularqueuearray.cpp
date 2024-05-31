8c2a44f143cc44fbe6b13daae4c82168d1688ff2
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-05-31 14:48:35 +0700

    9

--- circularqueuearray/circularqueuearray.cpp
+++ circularqueuearray/circularqueuearray.cpp
@@ -41,5 +41,11 @@ public:
 			return;
 		}
 		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+
 	}
 };
