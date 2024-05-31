eb5bdeadb26fa3c6b0b60526beabb1d3dc48ee42
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-05-31 14:33:20 +0700

    3

--- circularqueuearray/circularqueuearray.cpp
+++ circularqueuearray/circularqueuearray.cpp
@@ -11,3 +11,15 @@ public:
 		REAR = -1;
 	}
 
+	void insert() {
+		int num;
+		cout << "Enter a  number";
+		cin >> num;
+		cout << endl;
+
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
+	}
+};
